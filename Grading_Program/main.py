87bd01e378f7955581c37d0eb81f3f364f8c4a83
Andriy Zasypkin <Andy29485@users.noreply.github.com>
2015-11-03 18:26:33 -0500

    Compitition time/duration is now a thing

--- Grading_Program/main.py
+++ Grading_Program/main.py
@@ -1,12 +1,15 @@
 import sys
+import time
 import queue
 import Grader
 import argparse
 import ThreadGrader
 import SubmissionWatcher
 
+#read arguments from command line
 parser = argparse.ArgumentParser(description=__doc__)
 parser  .add_argument('submission_dir',   nargs='?', default='.',         help='directory to watch for submissions')
+parser  .add_argument('-t', '--duration',            default='04:00',     help='duration of cometition, format HH:MM')
 parser  .add_argument('-s', '--url',                 default='localhost', help='url of mysql server')
 parser  .add_argument('-u', '--username',            default='root',      help='username to mysql server')
 parser  .add_argument('-p', '--password',            default='password',  help='password to mysql server')
