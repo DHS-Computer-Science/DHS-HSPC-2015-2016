01d5beeb45900255e7ae2fed49271c9f92707432
noahdavis319 <noahdavis319@gmail.com>
2015-11-03 16:39:11 -0500

    Removed spaghetti code with connections variable
    Removed copy-pasted code that recurred exactly the same in 4 different
    files with a connections file that will create a $conn variable to run
    SQL query commands with

--- Submission_Program/createaccount.php
+++ Submission_Program/createaccount.php
@@ -1,18 +1,8 @@
 <?php
 
 if (isset($_GET["name"])) {
-	// Connection variables
-	$servername = "localhost";
-	$connusername = "root";
-	$connpassword = "password";
-
-	// Create connection
-	$conn = new mysqli($servername, $connusername, $connpassword, "hspc");
-
-	// Check connection
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
+	
+	include("connections.php");
 
 	// Get the username
 	$username = $_GET["name"];
