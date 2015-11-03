b30b01ae1a4c34a6d609ece3b6bba36397470854
Andriy Zasypkin <Andy29485@users.noreply.github.com>
2015-11-02 20:49:02 -0500

    Main Grader now has code in it
    https://xkcd.com/1296/

--- Grading_Program/main.py
+++ Grading_Program/main.py
@@ -1,2 +1,28 @@
 import queue
 import grader
+import threading
+
+q = queue.Queue()
+
+class ThreadGrade(threading.Thread):
+  """Threaded Url Grab"""
+  def __init__(self, q):
+    threading.Thread.__init__(self)
+    self.queue = q
+  
+  def run(self):
+    while True:
+      #grabs job from queue
+      submission = self.queue.get()
+      
+      #TODO - process queue
+      
+      #TODO - updates results?   maybe in another thread/queue or program
+      #TODO - send back results? maybe in another thread/queue or program
+      
+      #signals to queue job is done
+      self.queue.task_done()
+
+while True:
+  #TODO - add submissions to queue
+  #queue.put(...) #wait... what is it exactly that get put here? #TODO
