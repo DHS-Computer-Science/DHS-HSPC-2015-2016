56a7d1742d0a3fdde96b12389ae9dacdd4c57f23
Andriy Zasypkin <Andy29485@users.noreply.github.com>
2015-11-03 18:01:31 -0500

    Added arguments to pass to program on start-up

--- Grading_Program/main.py
+++ Grading_Program/main.py
@@ -1,7 +1,19 @@
+import sys
 import queue
 import Grader
+import argparse
 import ThreadGrader
 import SubmissionWatcher
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser  .add_argument('sbumission_dir',   nargs='?', default='.',         help='directory to watch for submissions')
+parser  .add_argument('-s', '--url',                 default='localhost', help='url of mysql server')
+parser  .add_argument('-u', '--username',            default='root',      help='username to mysql server')
+parser  .add_argument('-p', '--password',            default='password',  help='password to mysql server')
+parser  .add_argument('-d', '--database',            default='teams',     help='database to connect to(of the mysql server)')
+                                            #TODO change teams ^ to the value that should be used
+                                            #TODO add a config_file argument
+args   = parser.parse_args()
 
 q = queue.Queue()
 
