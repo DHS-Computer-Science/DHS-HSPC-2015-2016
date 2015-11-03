7f9d4dccb8fce5527c2008587686847182682daf
noahdavis319 <noahdavis319@gmail.com>
2015-11-03 16:14:31 -0500

    Formatting

--- Submission_Program/logout.php
+++ Submission_Program/logout.php
@@ -1,13 +1,17 @@
 <?php
+	
+	// Unset and expire the cookie
 	if (isset($_COOKIE["n"])) {
 		unset($_COOKIE["n"]);
 		setcookie("n","",time()-1);
 	}
 	
+	// Unset and expire the cookie
 	if (isset($_COOKIE["p"])) {
 		unset($_COOKIE["p"]);
 		setcookie("p","",time()-1);
 	}
-		
+	
+	// Navigate to the index page, it will handle redirections
 	header("Location: index.php");
 ?>
