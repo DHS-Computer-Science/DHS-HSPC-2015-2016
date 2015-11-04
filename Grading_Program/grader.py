bfce63a7947b8900c572465f3a7e5d5f8ac50fdb
Andriy Zasypkin <Andy29485@users.noreply.github.com>
2015-11-03 20:05:29 -0500

    changed default submission directory

--- Grading_Program/grader.py
+++ Grading_Program/grader.py
@@ -7,12 +7,12 @@ import argparse
 if __name__ == '__main__':
   #read arguments from command line
   parser = argparse.ArgumentParser(description=__doc__)
-  parser  .add_argument('submission_dir',   nargs='?', default='.',         help='directory to watch for submissions')
-  parser  .add_argument('-t', '--duration',            default='04:00',     help='duration of cometition, format HH:MM')
-  parser  .add_argument('-s', '--url',                 default='localhost', help='url of mysql server')
-  parser  .add_argument('-u', '--username',            default='root',      help='username to mysql server')
-  parser  .add_argument('-p', '--password',            default='password',  help='password to mysql server')
-  parser  .add_argument('-d', '--database',            default='teams',     help='database to connect to(of the mysql server)')
+  parser  .add_argument('submission_dir',   nargs='?', default='C:/xampp/submissions', help='directory to watch for submissions')
+  parser  .add_argument('-t', '--duration',            default='04:00',                help='duration of cometition, format HH:MM')
+  parser  .add_argument('-s', '--url',                 default='localhost',            help='url of mysql server')
+  parser  .add_argument('-u', '--username',            default='root',                 help='username to mysql server')
+  parser  .add_argument('-p', '--password',            default='password',             help='password to mysql server')
+  parser  .add_argument('-d', '--database',            default='teams',                help='database to connect to(of the mysql server)')
                                               #TODO change teams ^ to the value that should be used
                                               #TODO add a config_file argument
   args   = parser.parse_args()
