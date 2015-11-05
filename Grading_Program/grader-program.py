7f27e6032cb8ef88afa20435d73b9e8416b9a1d3
Andriy Zasypkin <Andy29485@users.noreply.github.com>
2015-11-05 18:36:54 -0500

    Merged config with args
    - only if config was specified in args
    - confing values take precedence over arg values
    
    Note: wow that took a lot of code out(I guess it was garbage code)

--- Grading_Program/grader-program.py
+++ Grading_Program/grader-program.py
@@ -3,6 +3,28 @@
 import main
 import sys
 import argparse
+from configparser import ConfigParser
+
+def read_db_config(filename='config.ini', section='mysql'):
+  """ Read database configuration file and return a dictionary object
+  :param filename: name of the configuration file
+  :param section: section of database configuration
+  :return: a dictionary of database parameters
+  """
+  # create parser and read ini configuration file
+  parser = ConfigParser()
+  parser.read(filename)
+
+  # get section, default to mysql
+  db = {}
+  if parser.has_section(section):
+    items = parser.items(section)
+    for item in items:
+      db[item[0]] = item[1]
+  else:
+    raise Exception('{0} not found in the {1} file'.format(section, filename))
+
+  return db
 
 if __name__ == '__main__':
   #read arguments from command line
@@ -17,8 +39,9 @@ if __name__ == '__main__':
   parser  .add_argument('-o', '--problems',            default='C:/problems',          help='directory with problem input/output files')
   parser  .add_argument('-s', '--submission',          default='C:/xampp/submissions', help='directory to watch for submissions')
 
-  #TODO use `vars(parser.parse_args())` to turn args into a dictionary
-  args   = parser.parse_args()
+  args = vars(parser.parse_args())
 
-  #TODO - mergre conf here
+  if args['config']:
+    args.update(read_db_config(args['config'], 'grader'))
+
   main.main(args)
--- Grading_Program/main.py
+++ Grading_Program/main.py
@@ -3,41 +3,14 @@ import queue
 import datetime
 import mysql.connector
 from Grader import Grader
-from configparser import ConfigParser
 from ThreadGrader import ThreadGrader
 from SubmissionWatcher import SubmissionWatcher
 from watchdog.observers import Observer
 
-def read_db_config(filename='config.ini', section='mysql'):
-  """ Read database configuration file and return a dictionary object
-  :param filename: name of the configuration file
-  :param section: section of database configuration
-  :return: a dictionary of database parameters
-  """
-  # create parser and read ini configuration file
-  parser = ConfigParser()
-  parser.read(filename)
-
-  # get section, default to mysql
-  db = {}
-  if parser.has_section(section):
-    items = parser.items(section)
-    for item in items:
-      db[item[0]] = item[1]
-  else:
-    raise Exception('{0} not found in the {1} file'.format(section, filename))
-
-  return db
-
 def main(args):
-  grader_conf = {}
-  #TODO - merge 'args' and the config
-  if args.config:
-    grader_conf = read_db_config(args.config, 'grader')
-
   #establish compitition start time
   start_time = time.time()
-  #TODO - use config file
+  #TODO - fix this ugly mess
   end_time   = datetime.timedelta(hours=int(args.duration.partition(':')[0]),
                                   minutes=int(args.duration.partition(':')[2])).total_seconds() + start_time
   #create a queue
@@ -46,13 +19,11 @@ def main(args):
   #connect to mysql server
   try:
     conf = {
-      'user'    : args.username,
-      'password': args.password,
-      'host'    : args.host,
-      'database': args.database
+      'user'    : args['username'],
+      'password': args['password'],
+      'host'    : args['host'],
+      'database': args['database']
     }
-    if args.config:
-      conf.update(read_db_config(args.config))
 
     cnx = mysql.connector.connect(conf)
   except Error as e:
@@ -63,20 +34,13 @@ def main(args):
 
   #file watcher
   observer = Observer()
-  if 'submission_dir' in grader_conf.keys():
-    observer.schedule(SubmissionWatcher(), path=grader_conf['submission_dir'])
-  else:
-    observer.schedule(SubmissionWatcher(), path=args.submission_dir)
+  observer.schedule(SubmissionWatcher(), path=args['submission_dir'])
   observer.start()
 
   #grader manager
-  #TODO - send the test_dir(the one with the problems)
-  if 'table' in grader_conf.keys():
-    grade_manager = ThreadGrader(q, cnx, grader_conf['table']) #create - w/ config file
-  else:
-    grade_manager = ThreadGrader(q, cnx, args.table) #create - w/o config file
-  grade_manager.setDaemon(True)   #do not exit until all things needed to be graded are graded
-  grade_manager.start()
+  grade_manager = ThreadGrader(q, cnx, arg['table']) #create - w/o config file
+  grade_manager.setDaemon(True) #do not exit until all things needed
+  grade_manager.start()         #  to be graded are graded, and start it
 
   #wait until end of compitition
   while time.time() <= end_time + 50: #give 50 second leeway
