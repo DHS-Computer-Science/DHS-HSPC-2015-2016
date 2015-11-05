661456d0d3ed6e83c7ab99e9dd62be8a5cf68cd2
Andriy Zasypkin <Andy29485@users.noreply.github.com>
2015-11-05 18:25:40 -0500

    added/fixed args

--- Grading_Program/grader-program.py
+++ Grading_Program/grader-program.py
@@ -7,15 +7,15 @@ import argparse
 if __name__ == '__main__':
   #read arguments from command line
   parser = argparse.ArgumentParser(description=__doc__)
-  parser  .add_argument('submission_dir',   nargs='?', default='C:/xampp/submissions', help='directory to watch for submissions')
   parser  .add_argument('-c', '--config',              default=None,                   help='config file')
   parser  .add_argument('-e', '--duration',            default='04:00',                help='duration of cometition, format HH:MM')
-  parser  .add_argument('-s', '--host',                default='localhost',            help='url of mysql server')
+  parser  .add_argument('-i', '--host',                default='localhost',            help='url of mysql server')
   parser  .add_argument('-u', '--username',            default='root',                 help='username to mysql server')
   parser  .add_argument('-p', '--password',            default='password',             help='password to mysql server')
   parser  .add_argument('-d', '--database',            default='teams',                help='database to connect to(of the mysql server)')
   parser  .add_argument('-t', '--table',               default='submissions',          help='table to update in database')
-  #TODO - add test dir arg(ya'know 'e on' wit 'e pro'lems)
+  parser  .add_argument('-o', '--problems',            default='C:/problems',          help='directory with problem input/output files')
+  parser  .add_argument('-s', '--submission',          default='C:/xampp/submissions', help='directory to watch for submissions')
 
   #TODO use `vars(parser.parse_args())` to turn args into a dictionary
   args   = parser.parse_args()
