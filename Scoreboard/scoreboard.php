728e385da993f492989e3930df3021e77db8d9e6
noahdavis319 <noahdavis319@gmail.com>
2015-11-10 16:50:56 -0500

    Added stock HTML
    Basic HTML code added, removed PHP "Hello World"

--- Scoreboard/scoreboard.php
+++ Scoreboard/scoreboard.php
@@ -1,4 +1,20 @@
 <?php
 	include("connections.php");
-	echo "Hello World!";
 ?>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="styles.css">
+	</head>
+	<body>
+		<div id="header">
+			<div class="navbutton">
+				LCPS HSPC
+			</div>
+		</div>
+		<div id="content" style="display: inline-block; text-align: center;">
+			<div id="small">
+
+			</div>
+		</div>
+	<body>
+</html>
