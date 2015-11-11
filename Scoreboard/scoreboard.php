7d7edc05571250b4aff6dad2cef0ef310292d30e
noahdavis319 <noahdavis319@gmail.com>
2015-11-11 08:25:50 -0500

    Retrieve problem data
    PHP foreach and for loop to get all the submission data for every
    problem for every team. Now the data has to be parsed.

--- Scoreboard/scoreboard.php
+++ Scoreboard/scoreboard.php
@@ -3,6 +3,7 @@
 ?>
 <html>
 	<head>
+		<meta http-equiv="refresh" content="30" />
 		<link rel="stylesheet" type="text/css" href="styles.css">
 	</head>
 	<body>
@@ -12,8 +13,36 @@
 			</div>
 		</div>
 		<div id="content" style="display: inline-block; text-align: center;">
-			<div id="small">
-
+			<div class="text">
+			<?php
+				// This is here all of the work needs to be done
+				
+				// First get the teams in order and store them into a PHP array
+					// This array will then be looped in the correct order and the data will be parsed
+					
+				// Get all of the team_ids
+				// Does not need to be a prepared statement because we are not inputting any values
+				$sql = "SELECT team_id FROM teams";
+				$result = $conn->query($sql);
+				
+				// Create an array of team_ids
+				$teams = $result->fetch_array(MYSQLI_NUM);
+				
+				foreach ($teams as $team) {
+					// For loop that repeates 6 times for the 6 problems we have
+					for ($problem_id = 0; $problem_id < 5; $problem_id++) {
+						// Get all the submission data for each problem for each time
+						$stmt = $conn->prepare("SELECT * FROM submissions WHERE team_id=? AND problem_id=?");
+						$stmt->bind_param("si", $team, $problem_id);
+						$stmt->execute();
+						$res = $stmt->get_result();
+						while($row = $res->fetch_assoc()) {
+							// Each row is a different submission for this problem, for this team.
+							echo print_r($row) . "<hr>";
+						}
+					}
+				}
+			?>
 			</div>
 		</div>
 	<body>
