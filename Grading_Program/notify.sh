5aad2253c6d809f8761fd5b0862c5f90d46c7735
Andriy Zasypkin <Andy29485@users.noreply.github.com>
2016-02-28 12:33:14 -0500

    added notifier

---
+++ Grading_Program/notify.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+case $2 in
+  -1)
+    zenity  --info  --text "$1" --title="System wide message" --timeout=0 --width=350 --height=100
+    exit 0 
+  0)
+    message="you should not see this, tell the people patrolling that you got a \"status 0\""
+    ;;
+  1)
+    message="Good Job, it works!"
+    ;;
+  2)
+    message="formatting error"
+    ;;
+  3)
+    message="compile error"
+    ;;
+  4)
+    message="no main class found"
+    ;;
+  5)
+    message="run time error"
+    ;;
+  6)
+    message="ran for too long > 1min"
+    ;;
+  7)
+    message="outputs do not match"
+    ;;
+  *)
+    message="Something happend with your submission, we are tring to fix it"
+    ;;
+
+  
+esac
+zenity  --info  --text "$message" --title="Problem $1" --timeout=0 --width=350 --height=100
